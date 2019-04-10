b553dfdd823ceb13e7da806ee39c1d685dc3752b
DanManN <dnahimov@gmail.com>
2019-04-10 15:02:00 -0400

    include perl files

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import os
 
 from pyrouge.utils.file_utils import list_files
 
-
 data_files = list_files('pyrouge/tests/data')
 data_files = [p.replace('pyrouge/tests/', '') for p in data_files]
 script_files = [os.path.join('bin', s) for s in os.listdir('bin')]
@@ -15,19 +14,17 @@ setup(
     author_email='benjamin.heinzerling@h-its.org',
     packages=['pyrouge', 'pyrouge.utils', 'pyrouge.tests'],
     scripts=script_files,
-    #test_suite='pyrouge.test.suite',
+    # test_suite='pyrouge.test.suite',
     package_data={'pyrouge.tests': data_files},
-    url='https://github.com/noutenki/pyrouge',
+    url='https://gitlab.com/DanManN/pyrouge',
     license='LICENSE.txt',
     description='A Python wrapper for the ROUGE summarization evaluation'
-        ' package.',
+    ' package.',
     classifiers=[
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Topic :: Text Processing :: Linguistic'],
+        'License :: OSI Approved :: MIT License', 'Operating System :: OS Independent',
+        'Programming Language :: Python', 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3', 'Topic :: Text Processing :: Linguistic'
+    ],
     long_description=open('README.rst').read(),
-    )
+)
